e20e402e7d0580a228db221beab183f4e8c96095
shahabhamdani <king.hamdani.3@gmail.com>
2021-11-12 16:37:53 -0800

    "login-form-created"

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
 
 export default function HeaderComponent() {
     return (
--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Menu from '@material-ui/core/Menu'
