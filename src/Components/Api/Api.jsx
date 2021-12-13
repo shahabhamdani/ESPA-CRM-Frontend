e5ca3ae51e5d3f7d1a5ff327031b4934c15cc7cf
shahabhamdani <king.hamdani.3@gmail.com>
2021-12-13 18:51:22 +0500

    minorApiBugFixes

--- src/Components/Api/Api.jsx
+++ src/Components/Api/Api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://crmbackendapi-dev.ap-south-1.elasticbeanstalk.com/api",
+  baseURL: "http://crmbackendapi-dev.ap-south-1.elasticbeanstalk.com/api",
 });
