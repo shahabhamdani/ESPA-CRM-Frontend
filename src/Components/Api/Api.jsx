3b0f7bb6f8c4fac94d59a41c1afd4b23b15d097d
shahabhamdani <king.hamdani.3@gmail.com>
2021-12-15 19:21:32 +0500

    apiHttpsProblemSolved

--- src/Components/Api/Api.jsx
+++ src/Components/Api/Api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "//crmbackendapi-dev.ap-south-1.elasticbeanstalk.com/api",
+  baseURL: "https://api.espabuilders.com/api",
 });
